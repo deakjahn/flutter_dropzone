c3e1996646b2f3cdd12668843432209633db9075
Steve Gehrman <sgehrman@gmail.com>
2022-03-24 03:56:59 -0700

    works now

--- flutter_dropzone_web/lib/flutter_dropzone_plugin.dart
+++ flutter_dropzone_web/lib/flutter_dropzone_plugin.dart
@@ -32,7 +32,7 @@ class FlutterDropzonePlugin extends FlutterDropzonePlatform {
     });
 
     html.document.body!.append(html.ScriptElement()
-      ..src = 'packages/flutter_dropzone_web/assets/flutter_dropzone.js'
+      ..src = 'assets/packages/flutter_dropzone_web/assets/flutter_dropzone.js'
       ..type = 'application/javascript'
       ..defer = true);
   }
