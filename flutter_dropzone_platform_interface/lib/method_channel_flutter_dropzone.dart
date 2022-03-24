e047e12acda308a90568d8c6fe19f7b7da0e3be6
Steve Gehrman <sgehrman@gmail.com>
2022-03-24 04:12:27 -0700

    fix desktop

--- flutter_dropzone_platform_interface/lib/method_channel_flutter_dropzone.dart
+++ flutter_dropzone_platform_interface/lib/method_channel_flutter_dropzone.dart
@@ -11,71 +11,91 @@ import 'flutter_dropzone_platform_interface.dart';
 class MethodChannelFlutterDropzone extends FlutterDropzonePlatform {
   @override
   void init(Map<String, dynamic> params, {required int viewId}) {
-    throw UnsupportedError('DropzoneView: $defaultTargetPlatform is not supported');
+    throw UnsupportedError(
+        'DropzoneView: $defaultTargetPlatform is not supported');
   }
 
   @override
   Future<bool> setOperation(DragOperation operation, {required int viewId}) {
-    throw UnsupportedError('DropzoneView: $defaultTargetPlatform is not supported');
+    throw UnsupportedError(
+        'DropzoneView: $defaultTargetPlatform is not supported');
   }
 
   @override
   Future<bool> setCursor(CursorType cursor, {required int viewId}) {
-    throw UnsupportedError('DropzoneView: $defaultTargetPlatform is not supported');
+    throw UnsupportedError(
+        'DropzoneView: $defaultTargetPlatform is not supported');
   }
 
   @override
   Future<bool> setMIME(List<String> mime, {required int viewId}) {
-    throw UnsupportedError('DropzoneView: $defaultTargetPlatform is not supported');
+    throw UnsupportedError(
+        'DropzoneView: $defaultTargetPlatform is not supported');
   }
 
   @override
-  Future<List<dynamic>> pickFiles(bool multiple, {List<String> mime = const [], required int viewId}) {
-    throw UnsupportedError('DropzoneView: $defaultTargetPlatform is not supported');
+  Future<List<dynamic>> pickFiles(bool multiple,
+      {List<String> mime = const [], required int viewId}) {
+    throw UnsupportedError(
+        'DropzoneView: $defaultTargetPlatform is not supported');
   }
 
   @override
   Future<String> getFilename(dynamic htmlFile, {required int viewId}) async {
-    throw UnsupportedError('DropzoneView: $defaultTargetPlatform is not supported');
+    throw UnsupportedError(
+        'DropzoneView: $defaultTargetPlatform is not supported');
   }
 
   @override
   Future<int> getFileSize(dynamic htmlFile, {required int viewId}) async {
-    throw UnsupportedError('DropzoneView: $defaultTargetPlatform is not supported');
+    throw UnsupportedError(
+        'DropzoneView: $defaultTargetPlatform is not supported');
   }
 
   @override
   Future<String> getFileMIME(dynamic htmlFile, {required int viewId}) async {
-    throw UnsupportedError('DropzoneView: $defaultTargetPlatform is not supported');
+    throw UnsupportedError(
+        'DropzoneView: $defaultTargetPlatform is not supported');
   }
 
   @override
-  Future<DateTime> getFileLastModified(dynamic htmlFile, {required int viewId}) async {
-    throw UnsupportedError('DropzoneView: $defaultTargetPlatform is not supported');
+  Future<DateTime> getFileLastModified(dynamic htmlFile,
+      {required int viewId}) async {
+    throw UnsupportedError(
+        'DropzoneView: $defaultTargetPlatform is not supported');
   }
 
   @override
   Future<String> createFileUrl(dynamic htmlFile, {required int viewId}) async {
-    throw UnsupportedError('DropzoneView: $defaultTargetPlatform is not supported');
+    throw UnsupportedError(
+        'DropzoneView: $defaultTargetPlatform is not supported');
   }
 
   @override
   Future<bool> releaseFileUrl(String fileUrl, {required int viewId}) async {
-    throw UnsupportedError('DropzoneView: $defaultTargetPlatform is not supported');
+    throw UnsupportedError(
+        'DropzoneView: $defaultTargetPlatform is not supported');
   }
 
   @override
   Future<Uint8List> getFileData(dynamic htmlFile, {required int viewId}) async {
-    throw UnsupportedError('DropzoneView: $defaultTargetPlatform is not supported');
+    throw UnsupportedError(
+        'DropzoneView: $defaultTargetPlatform is not supported');
   }
 
   @override
-  Stream<List<int>> getFileStream(dynamic htmlFile, {required int viewId}) async* {
-    throw UnsupportedError('DropzoneView: $defaultTargetPlatform is not supported');
+  Stream<List<int>> getFileStream(dynamic htmlFile,
+      {required int viewId}) async* {
+    throw UnsupportedError(
+        'DropzoneView: $defaultTargetPlatform is not supported');
   }
 
   @override
-  Widget buildView(Map<String, dynamic> creationParams, Set<Factory<OneSequenceGestureRecognizer>>? gestureRecognizers, PlatformViewCreatedCallback onPlatformViewCreated) {
-    return Text('DropzoneView: $defaultTargetPlatform is not supported');
+  Widget buildView(
+      Map<String, dynamic> creationParams,
+      Set<Factory<OneSequenceGestureRecognizer>>? gestureRecognizers,
+      PlatformViewCreatedCallback onPlatformViewCreated) {
+    // show nothing on desktop/mobile
+    return const SizedBox();
   }
 }
