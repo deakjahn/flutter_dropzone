99e352e978222791ff4529859fca26f46f443685
huanghui1998hhh <1224991097@qq.com>
2022-06-09 14:15:54 +0800

    add folder support

--- flutter_dropzone/example/lib/generated_plugin_registrant.dart
+++ flutter_dropzone/example/lib/generated_plugin_registrant.dart
@@ -4,6 +4,7 @@
 
 // ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: depend_on_referenced_packages
 
 import 'package:flutter_dropzone_web/flutter_dropzone_plugin.dart';
 
