e7023c0371950ca1f466d1138ea92340cf1f8f99
David Peters <46222898+dxvid-pts@users.noreply.github.com>
2024-02-17 15:21:09 +0100

    initial version

--- flutter_dropzone_web/lib/assets/flutter_dropzone.js
+++ flutter_dropzone_web/lib/assets/flutter_dropzone.js
@@ -1,129 +1,137 @@
 if (typeof FlutterDropzone === 'undefined') {
-class FlutterDropzone {
-  constructor(container, onLoaded, onError, onHover, onDrop, onDropInvalid, onDropMultiple, onLeave) {
-    this.onError = onError;
-    this.onHover = onHover;
-    this.onDrop = onDrop;
-    this.onDropInvalid = onDropInvalid;
-    this.onDropMultiple = onDropMultiple;
-    this.onLeave = onLeave;
-    this.dropMIME = null;
-    this.dropOperation = 'copy';
-
-    container.addEventListener('dragover', this.dragover_handler.bind(this));
-    container.addEventListener('dragleave', this.dragleave_handler.bind(this));
-    container.addEventListener('drop', this.drop_handler.bind(this));
-
-    if (onLoaded != null) onLoaded();
-  }
-
-  updateHandlers(onLoaded, onError, onHover, onDrop, onDropInvalid, onDropMultiple, onLeave) {
-    this.onError = onError;
-    this.onHover = onHover;
-    this.onDrop = onDrop;
-    this.onDropMultiple = onDropMultiple;
-    this.onDropInvalid = onDropInvalid;
-    this.onLeave = onLeave;
-    this.dropMIME = null;
-    this.dropOperation = 'copy';
-  }
-
-  dragover_handler(event) {
-    event.preventDefault();
-    event.dataTransfer.dropEffect = this.dropOperation;
-    if (this.onHover != null) this.onHover(event);
-  }
-
-  dragleave_handler(event) {
-    event.preventDefault();
-    if (this.onLeave != null) this.onLeave(event);
-  }
-
-  async drop_handler(event) {
-    event.preventDefault();
-
-    var files = [];
-    var strings = [];
-    if (event.dataTransfer.items) {
-      for (var i = 0; i < event.dataTransfer.items.length; i++) {
-        var item = event.dataTransfer.items[i];
-        switch (item.kind) {
-          case "file":
-            if (this.dropMIME == null || this.dropMIME.includes(item.type)) {
-              var file = item.getAsFile();
-              if (this.onDrop != null) this.onDrop(event, file);
-              files.push(file);
-            }
-            else {
-              if (this.onDropInvalid != null) this.onDropInvalid(event, item.type);
-            }
-            break;
-
-          case "string":
-            const that = this;
-            var text = await this.#getItemAsString(item);
-            if (that.onDrop != null) that.onDrop(event, text);
-            strings.push(text);
-            break;
-
-          default:
-            if (this.onError != null) this.onError("Wrong type: ${item.kind}");
-            break;
+  class FlutterDropzone {
+    constructor(container, onLoaded, onError, onHover, onDrop, onDropInvalid, onDropMultiple, onLeave) {
+      this.onError = onError;
+      this.onHover = onHover;
+      this.onDrop = onDrop;
+      this.onDropInvalid = onDropInvalid;
+      this.onDropMultiple = onDropMultiple;
+      this.onLeave = onLeave;
+      this.dropMIME = null;
+      this.dropOperation = 'copy';
+  
+      container.addEventListener('dragover', this.dragover_handler.bind(this));
+      container.addEventListener('dragleave', this.dragleave_handler.bind(this));
+      container.addEventListener('drop', this.drop_handler.bind(this));
+  
+      if (onLoaded != null) onLoaded();
+    }
+  
+    updateHandlers(onLoaded, onError, onHover, onDrop, onDropInvalid, onDropMultiple, onLeave) {
+      this.onError = onError;
+      this.onHover = onHover;
+      this.onDrop = onDrop;
+      this.onDropMultiple = onDropMultiple;
+      this.onDropInvalid = onDropInvalid;
+      this.onLeave = onLeave;
+      this.dropMIME = null;
+      this.dropOperation = 'copy';
+    }
+  
+    dragover_handler(event) {
+      event.preventDefault();
+      event.dataTransfer.dropEffect = this.dropOperation;
+      if (this.onHover != null) this.onHover(event);
+    }
+  
+    dragleave_handler(event) {
+      event.preventDefault();
+      if (this.onLeave != null) this.onLeave(event);
+    }
+  
+    async drop_handler(event) {
+      event.preventDefault();
+  
+      var files = [];
+      var strings = [];
+      if (event.dataTransfer.items) {
+        for (var i = 0; i < event.dataTransfer.items.length; i++) {
+          var item = event.dataTransfer.items[i];
+          switch (item.kind) {
+            case "file":
+              if (this.dropMIME == null || this.dropMIME.includes(item.type)) {
+                var file = item.getAsFile();
+                if (this.onDrop != null) this.onDrop(event, file);
+                files.push(file);
+              }
+              else {
+                if (this.onDropInvalid != null) this.onDropInvalid(event, item.type);
+              }
+              break;
+  
+            case "string":
+              const that = this;
+              var text = await this.#getItemAsString(item);
+              if (that.onDrop != null) that.onDrop(event, text);
+              strings.push(text);
+              break;
+  
+            default:
+              if (this.onError != null) this.onError("Wrong type: ${item.kind}");
+              break;
+          }
         }
+      } else {
+        for (var i = 0; i < ev.dataTransfer.files.length; i++)
+          var file = event.dataTransfer.files[i];
+          if (this.onDrop != null) this.onDrop(event, file);
+          files.push(file);
       }
-    } else {
-      for (var i = 0; i < ev.dataTransfer.files.length; i++)
-        var file = event.dataTransfer.files[i];
-        if (this.onDrop != null) this.onDrop(event, file);
-        files.push(file);
+  
+      if (this.onDropMultiple != null) {
+        if (files.length > 0) this.onDropMultiple(event, files);
+        if (strings.length > 0) this.onDropMultiple(event, strings);
+      }
     }
-
-    if (this.onDropMultiple != null) {
-      if (files.length > 0) this.onDropMultiple(event, files);
-      if (strings.length > 0) this.onDropMultiple(event, strings);
+  
+    #getItemAsString(item) {
+      return new Promise((resolve, reject) => {
+        item.getAsString(function (text) {
+          resolve(text);
+        });
+      })
+    }
+  
+    setMIME(mime) {
+      this.dropMIME = mime;
+    }
+  
+    setOperation(operation) {
+      this.dropOperation = operation;
     }
   }
-
-  #getItemAsString(item) {
-    return new Promise((resolve, reject) => {
-      item.getAsString(function (text) {
-        resolve(text);
-      });
-    })
+  
+  var flutter_dropzone_web = {
+    setMIME: function(container, mime) {
+      container.FlutterDropzone.setMIME(mime);
+      return true;
+    },
+  
+    setOperation: function(container, operation) {
+      container.FlutterDropzone.setOperation(operation);
+      return true;
+    },
+  
+    setCursor: function(container, cursor) {
+      container.style.cursor = cursor;
+      return true;
+    },
+  
+    create: function(container, onLoaded, onError, onHover, onDrop, onDropInvalid, onDropMultiple, onLeave) {
+      if (container.FlutterDropzone === undefined)
+        container.FlutterDropzone = new FlutterDropzone(container, onLoaded, onError, onHover, onDrop, onDropInvalid, onDropMultiple, onLeave);
+      else
+        container.FlutterDropzone.updateHandlers(onLoaded, onError, onHover, onDrop, onDropInvalid, onDropMultiple, onLeave);
+    },
+  
+    createObjectUrl: function(blob) {
+      return URL.createObjectURL(blob);
+    },
+  
+    revokeObjectUrl: function(url) {
+      URL.revokeObjectUrl(url);
+    },
+  };
+  
+  window.dispatchEvent(new Event('flutter_dropzone_web_ready'));
   }
-
-  setMIME(mime) {
-    this.dropMIME = mime;
-  }
-
-  setOperation(operation) {
-    this.dropOperation = operation;
-  }
-}
-
-var flutter_dropzone_web = {
-  setMIME: function(container, mime) {
-    container.FlutterDropzone.setMIME(mime);
-    return true;
-  },
-
-  setOperation: function(container, operation) {
-    container.FlutterDropzone.setOperation(operation);
-    return true;
-  },
-
-  setCursor: function(container, cursor) {
-    container.style.cursor = cursor;
-    return true;
-  },
-
-  create: function(container, onLoaded, onError, onHover, onDrop, onDropInvalid, onDropMultiple, onLeave) {
-    if (container.FlutterDropzone === undefined)
-      container.FlutterDropzone = new FlutterDropzone(container, onLoaded, onError, onHover, onDrop, onDropInvalid, onDropMultiple, onLeave);
-    else
-      container.FlutterDropzone.updateHandlers(onLoaded, onError, onHover, onDrop, onDropInvalid, onDropMultiple, onLeave);
-  },
-};
-
-window.dispatchEvent(new Event('flutter_dropzone_web_ready'));
-}
--- flutter_dropzone_web/lib/flutter_dropzone_web.dart
+++ flutter_dropzone_web/lib/flutter_dropzone_web.dart
@@ -2,48 +2,51 @@
 library flutter_dropzone_web;
 
 import 'dart:async';
-import 'dart:html';
+import 'dart:js_interop';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter_dropzone_platform_interface/flutter_dropzone_platform_interface.dart';
-import 'package:js/js.dart';
+import 'package:js/js.dart' as js;
+import 'package:web/web.dart' as web;
 
 class FlutterDropzoneView {
   final int viewId;
-  late final DivElement container;
+  late final web.HTMLDivElement container;
   List<String>? mime;
   DragOperation? operation;
   CursorType? cursor;
 
   FlutterDropzoneView(this.viewId) {
     final id = 'dropzone-container-$viewId';
-    container = DivElement()
+    container = web.HTMLDivElement()
       ..id = id
       ..style.pointerEvents = 'auto'
       ..style.border = 'none'
       // idea from https://keithclark.co.uk/articles/working-with-elements-before-the-dom-is-ready/
-      ..append(StyleElement()
+      ..append(web.HTMLStyleElement()
         ..innerText =
             '@keyframes $id-animation {from { clip: rect(1px, auto, auto, auto); } to { clip: rect(0px, auto, auto, auto); }}')
       ..style.animationName = '$id-animation'
       ..style.animationDuration = '0.001s'
       ..style.width = '100%'
       ..style.height = '100%'
-      ..addEventListener('animationstart', (event) {
-        _nativeCreate(
-          container,
-          allowInterop(_onLoaded),
-          allowInterop(_onError),
-          allowInterop(_onHover),
-          allowInterop(_onDrop),
-          allowInterop(_onDropInvalid),
-          allowInterop(_onDropMultiple),
-          allowInterop(_onLeave),
-        );
-        if (mime != null) setMIME(mime!);
-        if (operation != null) setOperation(operation!);
-        if (cursor != null) setCursor(cursor!);
-      });
+      ..addEventListener('animationstart', _startCallback.toJS);
+  }
+
+  void _startCallback(web.Event event) {
+    createJS(
+      container,
+      js.allowInterop(_onLoaded),
+      js.allowInterop(_onError),
+      js.allowInterop(_onHover),
+      js.allowInterop(_onDrop),
+      js.allowInterop(_onDropInvalid),
+      js.allowInterop(_onDropMultiple),
+      js.allowInterop(_onLeave),
+    );
+    if (mime != null) setMIME(mime!);
+    if (operation != null) setOperation(operation!);
+    if (cursor != null) setCursor(cursor!);
   }
 
   void init(Map<String, dynamic> params) {
@@ -53,82 +56,95 @@ class FlutterDropzoneView {
   }
 
   Future<bool> setMIME(List<String> mime) async {
-    return _nativeSetMIME(container, mime);
+    return setMimeJS(container, mime);
   }
 
   Future<bool> setOperation(DragOperation operation) async {
-    return _nativeSetOperation(container, describeEnum(operation));
+    return setOperationJS(container, operation.name);
   }
 
   Future<bool> setCursor(CursorType cursor) async {
-    return _nativeSetCursor(
-        container, describeEnum(cursor).toLowerCase().replaceAll('_', '-'));
+    return setCursorJS(
+        container, cursor.name.toLowerCase().replaceAll('_', '-'));
   }
 
   Future<List<dynamic>> pickFiles(bool multiple, List<String> mime) {
     final completer = Completer<List<dynamic>>();
-    final picker = FileUploadInputElement();
+    final picker = web.HTMLInputElement();
     final isSafari =
-        window.navigator.userAgent.toLowerCase().contains('safari');
-    if (isSafari) document.body!.append(picker);
+        web.window.navigator.userAgent.toLowerCase().contains('safari');
+    if (isSafari) web.document.body!.append(picker);
     picker.multiple = multiple;
     if (mime.isNotEmpty) picker.accept = mime.join(',');
-    picker.onChange.listen((_) {
-      completer.complete(picker.files);
+
+    void onChangeHandler() {
+      if (picker.files != null) {
+        final list = List.generate(
+            picker.files!.length, (index) => picker.files!.item(index));
+        completer.complete(list);
+      }
+      else
+        completer.complete([]);
       if (isSafari) picker.remove();
-    });
-    picker.on['cancel'].listen((_) {
+    }
+
+    void onCancelHandler() {
       completer.complete([]);
       if (isSafari) picker.remove();
-    });
+    }
+
+    picker.onchange = onChangeHandler.toJS;
+    picker.oncancel = onCancelHandler.toJS;
     picker.click();
     return completer.future;
   }
 
-  Future<String> getFilename(File file) async {
+  Future<String> getFilename(web.File file) async {
     return file.name;
   }
 
-  Future<int> getFileSize(File file) async {
+  Future<int> getFileSize(web.File file) async {
     return file.size;
   }
 
-  Future<String> getFileMIME(File file) async {
+  Future<String> getFileMIME(web.File file) async {
     return file.type;
   }
 
-  Future<DateTime> getFileLastModified(File file) async {
-    return file.lastModified != null
-        ? DateTime.fromMillisecondsSinceEpoch(file.lastModified!)
-        : file.lastModifiedDate;
+  Future<DateTime> getFileLastModified(web.File file) async {
+    return DateTime.fromMillisecondsSinceEpoch(file.lastModified);
   }
 
-  Future<String> createFileUrl(File file) async {
-    return Url.createObjectUrlFromBlob(file);
+  Future<String> createFileUrl(web.File file) async {
+    return createObjectUrlJS(file);
   }
 
   Future<bool> releaseFileUrl(String fileUrl) async {
-    Url.revokeObjectUrl(fileUrl);
+    revokeObjectUrlJS(fileUrl);
     return true;
   }
 
-  Future<Uint8List> getFileData(File file) async {
-    final completer = Completer<Uint8List>();
-    final reader = FileReader();
-    reader.readAsArrayBuffer(file);
-    reader.onLoad.listen((_) => completer.complete(reader.result as Uint8List));
-    return completer.future;
+  Future<Uint8List> getFileData(web.File file) async {
+    return file.arrayBuffer() as Uint8List;
+    // final completer = Completer<Uint8List>();
+    // final reader = web.FileReader();
+    // reader.readAsArrayBuffer(file);
+    // reader.onload = (_) {
+    //   completer.complete(reader.result as Uint8List);
+    // }.toJS;
+    // return completer.future;
   }
 
-  Stream<List<int>> getFileStream(File file) async* {
+  Stream<List<int>> getFileStream(web.File file) async* {
+    // final reader = file.stream().getReader();
     const int chunkSize = 1024 * 1024;
-    final reader = FileReader();
+    final reader = web.FileReader();
     int start = 0;
     while (start < file.size) {
       final end = start + chunkSize > file.size ? file.size : start + chunkSize;
       final blob = file.slice(start, end);
       reader.readAsArrayBuffer(blob);
-      await reader.onLoad.first;
+      await reader.onLoadEnd.first; //???
       yield reader.result as List<int>;
       start += chunkSize;
     }
@@ -140,28 +156,28 @@ class FlutterDropzoneView {
   void _onError(String error) => FlutterDropzonePlatform.instance.events
       .add(DropzoneErrorEvent(viewId, error));
 
-  void _onHover(MouseEvent event) =>
+  void _onHover(web.MouseEvent event) =>
       FlutterDropzonePlatform.instance.events.add(DropzoneHoverEvent(viewId));
 
-  void _onDrop(MouseEvent event, dynamic data) =>
+  void _onDrop(web.MouseEvent event, dynamic data) =>
       FlutterDropzonePlatform.instance.events
           .add(DropzoneDropEvent(viewId, data));
 
-  void _onDropInvalid(MouseEvent event, String mime) =>
+  void _onDropInvalid(web.MouseEvent event, String mime) =>
       FlutterDropzonePlatform.instance.events
           .add(DropzoneDropInvalidEvent(viewId, mime));
 
-  void _onDropMultiple(MouseEvent event, List<dynamic> data) =>
+  void _onDropMultiple(web.MouseEvent event, List<dynamic> data) =>
       FlutterDropzonePlatform.instance.events
           .add(DropzoneDropMultipleEvent(viewId, data));
 
-  void _onLeave(MouseEvent event) =>
+  void _onLeave(web.MouseEvent event) =>
       FlutterDropzonePlatform.instance.events.add(DropzoneLeaveEvent(viewId));
 }
 
-@JS('create')
-external void _nativeCreate(
-    dynamic container,
+@JS("create")
+external void createJS(
+    web.HTMLDivElement container,
     Function onLoaded,
     Function onError,
     Function onHover,
@@ -170,11 +186,17 @@ external void _nativeCreate(
     Function onDropMultiple,
     Function onLeave);
 
-@JS('setMIME')
-external bool _nativeSetMIME(dynamic container, List<String> mime);
+@JS("setMIME")
+external bool setMimeJS(web.HTMLDivElement container, List<String> mime);
 
-@JS('setOperation')
-external bool _nativeSetOperation(dynamic container, String operation);
+@JS("setOperation")
+external bool setOperationJS(web.HTMLDivElement container, String operation);
 
-@JS('setCursor')
-external bool _nativeSetCursor(dynamic container, String cursor);
+@JS("setCursor")
+external bool setCursorJS(web.HTMLDivElement container, String cursor);
+
+@JS("createObjectUrl")
+external String createObjectUrlJS(web.Blob object);
+
+@JS("revokeObjectUrl")
+external void revokeObjectUrlJS(String url);
